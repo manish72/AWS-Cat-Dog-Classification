e4e621175671cb14a3b9af92acc47286cab27327
manish72 <manishguru72@gmail.com>
2023-11-04 16:57:37 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="Cat-Dog",
     version="0.0.1",
-    author="iNeuron",
-    author_email="query@ineuron.ai",
+    author="GuruManish",
+    author_email="manishguru72@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
